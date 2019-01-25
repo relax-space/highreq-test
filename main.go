501ed6789852bdb05df0dcfb21459d6e1bfe364e
xiao.xinmiao <xiaoxm_001@163.com>
2019-01-25 08:37:40 +0800

    dockerfile1,2

--- main.go
+++ main.go
@@ -2,49 +2,75 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 	"os"
 	"strconv"
-	"time"
 
 	"github.com/chneau/limiter"
 
 	"github.com/labstack/echo"
 )
 
+type ParamDto struct {
+	Count int `json:"count" query:"count"`
+}
+
 func main() {
 	goRoutine := flag.String("GO_ROUTINE", os.Getenv("GO_ROUTINE"), "GO_ROUTINE")
-	taskCount := flag.String("TASK_COUNT", os.Getenv("TASK_COUNT"), "TASK_COUNT")
-
 	goRoutineCount, _ := strconv.ParseInt(*goRoutine, 10, 64)
-	count, _ := strconv.ParseInt(*taskCount, 10, 64)
 	e := echo.New()
 	e.GET("/ping", func(c echo.Context) error {
 		return c.String(http.StatusOK, "pong")
 	})
 	var limitEvent limiter.Limiter = limiter.New(int(goRoutineCount))
 	e.GET("/test1", func(c echo.Context) error {
+
 		limitEvent.Execute(func() {
-			for index := 0; index < int(count); index++ {
-				time.Sleep(20 * time.Second)
+			var v ParamDto
+			if err := c.Bind(&v); err != nil {
+				return
 			}
+			i := 0
+			for index := 0; index < v.Count; index++ {
+				for index2 := 0; index2 < 100000000; index2++ {
+					i += index2
+				}
+			}
+			fmt.Println(i)
 		})
 		return c.String(http.StatusOK, "success")
 	})
 
 	e.GET("/test2", func(c echo.Context) error {
-		for index := 0; index < int(count); index++ {
-			time.Sleep(20 * time.Second)
+		var v ParamDto
+		if err := c.Bind(&v); err != nil {
+			return c.String(http.StatusOK, "failure")
 		}
+		i := 0
+		for index := 0; index < v.Count; index++ {
+			for index2 := 0; index2 < 100000000; index2++ {
+				i += index2
+			}
+		}
+		fmt.Println(i)
 		return c.String(http.StatusOK, "success")
 	})
 
 	e.GET("/test3", func(c echo.Context) error {
-		go func(number int64) {
-			for index := 0; index < int(number); index++ {
-				time.Sleep(20 * time.Second)
+		go func(cA echo.Context) {
+			var v ParamDto
+			if err := cA.Bind(&v); err != nil {
+				return
 			}
-		}(count)
+			i := 0
+			for index := 0; index < v.Count; index++ {
+				for index2 := 0; index2 < 100000000; index2++ {
+					i += index2
+				}
+			}
+			fmt.Println(i)
+		}(c)
 		return c.String(http.StatusOK, "success")
 	})
 
